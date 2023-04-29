7b5acd291552a52cddbae554d477754bedf69a28
kawsarhamidi <kawsarhamidi77@gmail.com>
2023-04-29 19:44:09 +0600

    hide vite

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable ', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyAiZriLVJ-GQIY7ppc0Ba7OKrZb_PQC2MY",
-  authDomain: "the-news-dragon-910fa.firebaseapp.com",
-  projectId: "the-news-dragon-910fa",
-  storageBucket: "the-news-dragon-910fa.appspot.com",
-  messagingSenderId: "718767395347",
-  appId: "1:718767395347:web:6c44e8369028da4d63a5e6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
